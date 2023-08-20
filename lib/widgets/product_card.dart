013a7c9ad62c49258bab19a64339136c7f1a7ddc
pulkit-singhall <singhal.pulkit667@gmail.com>
2023-08-20 18:53:47 +0530

    Product Design

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -16,25 +16,53 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: index.isEven
+            ? const Color.fromRGBO(216, 240, 253, 1)
+            : const Color.fromRGBO(243, 243, 248, 1.0),
+      ),
       width: Checkbox.width,
-      color: index.isEven ? const Color.fromRGBO(216, 240, 253, 1) : const Color.fromRGBO(
-          230, 181, 151, 1.0),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(title),
-            const SizedBox(
-              height: 15,
+            Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 22,
+              ),
+              maxLines: 1,
+              textAlign: TextAlign.start,
             ),
-            Text('\$ $price'),
             const SizedBox(
-              height: 15,
+              height: 10,
             ),
-            Image(image: AssetImage(image),
-                height: 180,
-              width: 220,
+            Text(
+              '\$ $price',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage(image),
+                  alignment: Alignment.center,
+                  height: 180,
+                  width: 260,
+                )
+              ],
             ),
           ],
         ),
